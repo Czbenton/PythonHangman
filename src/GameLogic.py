9d4d96766eedae0321192fabce36358619dadff7
Zach Benton <Zach@Zach.local>
2017-02-10 13:24:08 -0600

    graphics copied over

--- src/GameLogic.py
+++ src/GameLogic.py
@@ -1,5 +1,8 @@
 import random
 
+rightGuesses = []
+wrongGuesses = []
+
 
 def selectRandomWord():
     array = []
@@ -10,3 +13,26 @@ def selectRandomWord():
     x = random.randrange(1, 354939)
     randomWord = array[x]
     return randomWord
+
+
+gameWord = list(selectRandomWord())
+
+
+def createGameBoard():
+    for x in range(len(gameWord)):
+        rightGuesses.append("_")
+
+
+def makeAGuess():
+    userGuess = input("Please enter a guess!\n")
+    if userGuess in gameWord:
+        for x in range(len(gameWord)):
+            if userGuess == gameWord[x]:
+                rightGuesses[x] = userGuess
+    else:
+        wrongGuesses.append(userGuess)
+        print("Wrong. Try again.", wrongGuesses)
+
+    print(rightGuesses)
+
+
--- src/Main.py
+++ src/Main.py
@@ -1,31 +1,11 @@
 import GameLogic
+import Graphics
 
-rightGuesses = []
-wrongGuesses = []
-gameWord = list(GameLogic.selectRandomWord())
+print(Graphics.board1, sep="\n")
 
+print(GameLogic.gameWord) #todo cheat mode
 
-def createGameBoard():
-    for x in range(len(gameWord)):
-        rightGuesses.append("_")
-
-
-def makeAGuess():
-    userGuess = input("Please enter a guess!\n")
-    if userGuess in gameWord:
-        for x in range(len(gameWord)):
-            if userGuess == gameWord[x]:
-                rightGuesses[x] = userGuess
-    else:
-        wrongGuesses.append(userGuess)
-        print("Wrong. Try again.", wrongGuesses)
-
-    print(rightGuesses)
-
-
-print(gameWord) #todo cheat mode
-
-createGameBoard()
-print(rightGuesses)
-while "_" in rightGuesses:
-    makeAGuess()
+GameLogic.createGameBoard()
+print(GameLogic.rightGuesses)
+while "_" in GameLogic.rightGuesses:
+    GameLogic.makeAGuess()
